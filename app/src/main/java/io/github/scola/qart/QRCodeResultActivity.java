a817382d069e91cdd0ae7f7e37fdd8388b3c8cb3
shaozheng <shaozheng.wu@gmail.com>
2025-11-24 09:50:16 +0800

    remove ss config related code

--- app/src/main/java/io/github/scola/qart/QRCodeResultActivity.java
+++ app/src/main/java/io/github/scola/qart/QRCodeResultActivity.java
@@ -43,9 +43,7 @@ public class QRCodeResultActivity extends AppCompatActivity {
     private final static int REQUEST_SAVE_FILE = 5;
 
     private String qrText;
-    private String ssText;
     private TextView mTextView;
-    private TextView mSsTextView;
     private ImageView mQRImage;
 
     @Override
@@ -59,7 +57,6 @@ public class QRCodeResultActivity extends AppCompatActivity {
 
         mTextView = (TextView) findViewById(R.id.qr_text);
         mQRImage = (ImageView) findViewById(R.id.qr_result_img);
-        mSsTextView = (TextView) findViewById(R.id.ss_text);
 
         qrText = getIntent().getExtras().getString("TEXT", "");
         if (qrText.isEmpty()) {
@@ -68,30 +65,7 @@ public class QRCodeResultActivity extends AppCompatActivity {
             mQRImage.setImageBitmap(CuteR.ProductForResult(qrText));
             mTextView.setOnClickListener(getClickListener(qrText, getResources().getStringArray(R.array.qr_list_uri)));
             mTextView.setText(qrText);
-            if (qrText.startsWith("ss://")) {
-                String ss = qrText.replace("ss://","");
-                try {
-                    ssText = "ss://" + new String(Base64.decode(ss, Base64.DEFAULT));
-                    if (ssText.contains("@") && ssText.split(":").length == 4) {
-                        mSsTextView.setVisibility(View.VISIBLE);
-                        mSsTextView.setText(ssText);
-                        mSsTextView.setOnClickListener(getClickListener(ssText, getResources().getStringArray(R.array.ss_list_uri)));
-                    }
-                } catch (IllegalArgumentException ex) {
-                    ex.printStackTrace();
-                }
-            }
-
         }
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     private View.OnClickListener getClickListener (final String text, final String[] items) {
@@ -116,9 +90,6 @@ public class QRCodeResultActivity extends AppCompatActivity {
         switch (pos) {
             case 0:
                 ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                if (ssText != null) {
-                    text = getAllSsConfig(ssText);
-                }
                 ClipData clip = ClipData.newPlainText("label", text);
                 clipboard.setPrimaryClip(clip);
                 Toast.makeText(this, R.string.qr_copied, Toast.LENGTH_SHORT).show();
@@ -132,9 +103,6 @@ public class QRCodeResultActivity extends AppCompatActivity {
             case 2:
                 Intent i = new Intent(android.content.Intent.ACTION_SEND);
                 i.setType("text/plain");
-                if (ssText != null) {
-                    text = getAllSsConfig(ssText);
-                }
                 i.putExtra(Intent.EXTRA_TEXT, text);
                 startActivity(Intent.createChooser(i, getResources().getString(R.string.share_channel)));
                 break;
@@ -162,99 +130,9 @@ public class QRCodeResultActivity extends AppCompatActivity {
                 }
 
                 break;
-            case 4:
-                if (isStoragePermissionGranted(REQUEST_SAVE_FILE)) {
-                    saveSSConfigInFile();
-                }
-                break;
-            case 5:
-                File shareFile = new File(getExternalCacheDir(), "ss_"+ text.split("@")[1].replace(":", "_") + ".json");
-                saveSSConfig(shareFile, text);
-
-                Uri contentUri = Uri.fromFile(shareFile);
-
-                if (contentUri != null) {
-                    Log.d(TAG, "Uri: " + contentUri);
-                    Intent shareIntent = new Intent();
-                    shareIntent.setAction(Intent.ACTION_SEND);
-                    shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    shareIntent.setType("application/json");
-                    shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
-                    startActivity(Intent.createChooser(shareIntent, getResources().getString(R.string.share_via)));
-                }
-                break;
 
             default:
                 break;
-        }
-    }
-
-    private void saveSSConfigInFile() {
-        File downloadFolder = new File(Environment.getExternalStorageDirectory(), "Download");
-        if (!downloadFolder.exists()) {
-            downloadFolder.mkdirs();
-        }
-
-        File ssConfig = new File(downloadFolder, "ss_"+ ssText.split("@")[1].replace(":", "_") + ".json");
-        saveSSConfig(ssConfig, ssText);
-        Toast.makeText(this, getResources().getString(R.string.saved) + ssConfig.getAbsolutePath(), Toast.LENGTH_LONG).show();
-    }
-
-    private String genShadowsocksConfig(String text) throws JSONException {
-        text = text.replace("ss://", "");
-        String[] secretServer = text.split("@");
-        String[] secret = secretServer[0].split(":");
-        String[] serverPort = secretServer[1].split(":");
-
-        JSONObject json = new JSONObject();
-
-        json.put("server", serverPort[0]);
-        json.put("server_port", Integer.parseInt(serverPort[1]));
-        json.put("local_port", 1080);
-        json.put("method", secret[0]);
-        json.put("password", secret[1]);
-        json.put("timeout", 300);
-
-        String configStr = json.toString();
-        String newStr = configStr.replaceFirst("\\{", "{\n\t");
-        newStr = newStr.replaceAll(",\"", ",\n\t\"");
-        newStr = newStr.substring(0, newStr.length() - 1) + "\n}";
-
-        return newStr;
-    }
-
-    private String getAllSsConfig(String text) {
-        StringBuilder sb = new StringBuilder(text);
-
-        String[] secretServer = text.split("@");
-        String[] secret = secretServer[0].replace("ss://", "").split(":");
-        String[] serverPort = secretServer[1].split(":");
-
-        sb.append("\n\n");
-        sb.append("sslocal");
-        sb.append(" -s " + serverPort[0]);
-        sb.append(" -p " + serverPort[1]);
-        sb.append(" -m " + secret[0]);
-        sb.append(" -k " + secret[1]);
-        sb.append(" -l " + 1080);
-        sb.append(" -v");
-
-        sb.append("\n\n");
-        try {
-            sb.append(genShadowsocksConfig(text));
-        } catch (JSONException ex) {
-            ex.printStackTrace();
-        }
-        return sb.toString();
-    }
-
-    private void saveSSConfig(File dst, String text) {
-        try {
-            Util.saveConfig(dst, genShadowsocksConfig(text));
-        } catch (JSONException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
     }
 
@@ -281,7 +159,6 @@ public class QRCodeResultActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
             Log.v(TAG,"Permission: "+ permissions[0] + "was "+ grantResults[0]);
-                saveSSConfigInFile();
 
         }
     }
